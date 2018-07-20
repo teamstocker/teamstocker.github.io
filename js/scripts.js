3529d23c1fb542794fefcaaec0aaf94746e10f08
thebauzz <32292938+thebauzz@users.noreply.github.com>
2018-07-20 07:33:05 +0200

    burgermenu -- "X" v180719-07.33

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
-    $('.menu-icon').on("click", function() {
+    $(".menu-icon").on("click", function() {
         nav_rein_raus();
-        $("header span").toggleClass("gedreht");
+        $("div.normal1").toggleClass("gedreht1");
+        $("div.normal2").toggleClass("gedreht2");
+        $("div.normal3").toggleClass("gedreht3");
     });
 });
 function nav_rein_raus() {
-    var button = $('header nav');
-    if (button.hasClass('inemitdem')) {
-        button.removeClass('inemitdem').addClass('usemitdem');
+    var button = $("header nav");
+    if (button.hasClass("inemitdem")) {
+        button.removeClass("inemitdem").addClass("usemitdem");
     }
-    else if (button.hasClass('usemitdem')) {
-        button.removeClass('usemitdem').addClass('inemitdem');
+    else if (button.hasClass("usemitdem")) {
+        button.removeClass("usemitdem").addClass("inemitdem");
     }
     else {
-        button.addClass('inemitdem');
+        button.addClass("inemitdem");
     }
 };
