a4577a823c0446124f467ad733922964a6566178
thebauzz <32292938+thebauzz@users.noreply.github.com>
2018-07-21 12:06:05 +0200

    evrth local - v18071-12.06

--- js/necessary.js
+++ js/necessary.js
@@ -17,6 +17,13 @@
 
 	if ( typeof module === "object" && typeof module.exports === "object" ) {
 
+		// For CommonJS and CommonJS-like environments where a proper `window`
+		// is present, execute the factory and get jQuery.
+		// For environments that do not have a `window` with a `document`
+		// (such as Node.js), expose a factory as module.exports.
+		// This accentuates the need for the creation of a real `window`.
+		// e.g. var jQuery = require("jquery")(window);
+		// See ticket #14549 for more info.
 		module.exports = global.document ?
 			factory( global, true ) :
 			function( w ) {
@@ -29,9 +36,13 @@
 		factory( global );
 	}
 
-
+// Pass this if window is not defined yet
 } )( typeof window !== "undefined" ? window : this, function( window, noGlobal ) {
 
+// Edge <= 12 - 13+, Firefox <=18 - 45+, IE 10 - 11, Safari 5.1 - 9+, iOS 6 - 9.1
+// throw exceptions when non-strict code (e.g., ASP.NET 4.5) accesses strict mode
+// arguments.callee.caller (trac-13335). But as of jQuery 3.0 (2016), strict mode should be common
+// enough that all such attempts are guarded in a try block.
 "use strict";
 
 var arr = [];
@@ -62,6 +73,10 @@ var support = {};
 
 var isFunction = function isFunction( obj ) {
 
+      // Support: Chrome <=57, Firefox <=52
+      // In some browsers, typeof returns "function" for HTML <object> elements
+      // (i.e., `typeof document.createElement( "object" ) === "function"`).
+      // We don't want to classify *any* DOM node as a function.
       return typeof obj === "function" && typeof obj.nodeType !== "number";
   };
 
@@ -102,51 +117,67 @@ function toType( obj ) {
 		return obj + "";
 	}
 
+	// Support: Android <=2.3 only (functionish RegExp)
 	return typeof obj === "object" || typeof obj === "function" ?
 		class2type[ toString.call( obj ) ] || "object" :
 		typeof obj;
 }
+/* global Symbol */
+// Defining this global in .eslintrc.json would create a danger of using the global
+// unguarded in another place, it seems safer to define global only for this module
+
+
+
 var
 	version = "3.3.1",
 
+	// Define a local copy of jQuery
 	jQuery = function( selector, context ) {
 
-
+		// The jQuery object is actually just the init constructor 'enhanced'
+		// Need init if jQuery is called (just allow error to be thrown if not included)
 		return new jQuery.fn.init( selector, context );
 	},
 
-
+	// Support: Android <=4.0 only
+	// Make sure we trim BOM and NBSP
 	rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
 
 jQuery.fn = jQuery.prototype = {
 
-
+	// The current version of jQuery being used
 	jquery: version,
 
 	constructor: jQuery,
 
+	// The default length of a jQuery object is 0
 	length: 0,
 
 	toArray: function() {
 		return slice.call( this );
 	},
 
-
+	// Get the Nth element in the matched element set OR
+	// Get the whole matched element set as a clean array
 	get: function( num ) {
 
+		// Return all the elements in a clean array
 		if ( num == null ) {
 			return slice.call( this );
 		}
 
+		// Return just the one element from the set
 		return num < 0 ? this[ num + this.length ] : this[ num ];
 	},
 
-
+	// Take an array of elements and push it onto the stack
+	// (returning the new matched element set)
 	pushStack: function( elems ) {
 
+		// Build a new jQuery matched element set
 		var ret = jQuery.merge( this.constructor(), elems );
 
-
+		// Add the old object onto the stack (as a reference)
 		ret.prevObject = this;
 
 		// Return the newly-formed element set
