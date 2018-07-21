76509bd710925243fd729466975b33eb5ecfb799
thebauzz <32292938+thebauzz@users.noreply.github.com>
2018-07-21 13:17:58 +0200

    FINALLY WITHOUT JQUERY - v180721-13.17

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,3 @@
-/* Menu Icon chan sich verändere, wenns drückt wird */
-$(document).ready(function() {
-    $(".menu-icon").on("click", function() {
-        nav_rein_raus();
-        $("div.normal1").toggleClass("gedreht1");
-        $("div.normal2").toggleClass("gedreht2");
-        $("div.normal3").toggleClass("gedreht3");
-    });
-});
-/* Dass D'navigation smooth ine und use gaht */
-function nav_rein_raus() {
-    var button = $("header nav");
-    if (button.hasClass("inemitdem")) {
-        button.removeClass("inemitdem").addClass("usemitdem");
-    }
-    else if (button.hasClass("usemitdem")) {
-        button.removeClass("usemitdem").addClass("inemitdem");
-    }
-    else {
-        button.addClass("inemitdem");
-    }
-};
 /* De Wiss schissdräck underem header Bild --> VIlL Z'LANG GHA FÜR DE GAGIFURZ!!!!!!!! */
 window.onresize = displayWindowSize;
 window.onload = displayWindowSize;
@@ -28,3 +6,30 @@ function displayWindowSize() {
   var koordinate = "0,50 " + breite + ",50 " + breite + ",0";
   document.getElementById("zeichnebitte").setAttribute("points", koordinate);
 };
+
+/*
+Okay, für die Funktion hani extrem lang gha. Umso stolzer, dassis ohni externi Library anebracht han!!!!!!
+die erste 3 Ziile mached, dass s'Hamburgermenu zumne X wird.
+De Rest füegt oder entfernt d'Animation zum NAV inetue oder usetue
+*/
+function myFunction() {
+
+  document.getElementById("normal1").classList.toggle("gedreht1");
+  document.getElementById("normal2").classList.toggle("gedreht2");
+  document.getElementById("normal3").classList.toggle("gedreht3");
+
+
+nav = document.getElementById("navfuerjs");
+
+  if (nav.classList.contains("inemitdem")) {
+    nav.classList.remove("inemitdem");
+    nav.classList.add("usemitdem");
+  }
+  else if (document.getElementById("navfuerjs").classList.contains("usemitdem")){
+    nav.classList.remove("usemitdem");
+    nav.classList.add("inemitdem");
+  }
+  else {
+    nav.classList.add("inemitdem");
+  }
+}
