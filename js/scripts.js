75b38a3d83238f834e808153cb18b45740efd2cc
thebauzz <32292938+thebauzz@users.noreply.github.com>
2018-07-22 09:07:28 +0200

    optimizing stuff - v180722-09.07

--- js/scripts.js
+++ js/scripts.js
@@ -12,20 +12,20 @@ Okay, für die Funktion hani extrem lang gha. Umso stolzer, dassis ohni externi 
 die erste 3 Ziile mached, dass s'Hamburgermenu zumne X wird.
 De Rest füegt oder entfernt d'Animation zum NAV inetue oder usetue
 */
-function myFunction() {
+
+function nav_ine_use() {
+
+  nav = document.getElementById("navfuerjs");
 
   document.getElementById("normal1").classList.toggle("gedreht1");
   document.getElementById("normal2").classList.toggle("gedreht2");
   document.getElementById("normal3").classList.toggle("gedreht3");
 
-
-nav = document.getElementById("navfuerjs");
-
   if (nav.classList.contains("inemitdem")) {
     nav.classList.remove("inemitdem");
     nav.classList.add("usemitdem");
   }
-  else if (document.getElementById("navfuerjs").classList.contains("usemitdem")){
+  else if (nav.classList.contains("usemitdem")){
     nav.classList.remove("usemitdem");
     nav.classList.add("inemitdem");
   }
