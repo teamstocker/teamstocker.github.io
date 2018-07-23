5caaaba426754bdb2d52be7aefa51b4f386e643c
thebauzz <32292938+thebauzz@users.noreply.github.com>
2018-07-23 11:54:20 +0200

    fixed isseu onresize - v180723-11.54

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,33 @@
-/* De Wiss schissdräck underem header Bild --> VIlL Z'LANG GHA FÜR DE GAGIFURZ!!!!!!!! */
+
 window.onresize = displayWindowSize;
 window.onload = displayWindowSize;
 function displayWindowSize() {
+
+/* De Wiss schissdräck underem header Bild --> VIlL Z'LANG GHA FÜR DE GAGIFURZ!!!!!!!! */
   breite = window.innerWidth;
   var koordinate = "0,50 " + breite + ",50 " + breite + ",0";
   document.getElementById("zeichnebitte").setAttribute("points", koordinate);
+
+/* Bilder wo uf de Teamsite sind, werded je nach Bildschirmgrössi veränderet */
+  Bildbreite = window.innerWidth;
+  if (Bildbreite > 900) {
+    document.getElementById("teambild").setAttribute("width", "300px")
+    document.getElementById("teambild2").setAttribute("width", "300px")
+    document.getElementById("teambild3").setAttribute("width", "300px")
+    document.getElementById("teambild4").setAttribute("width", "300px")
+  }
+  else if (Bildbreite < 900 && Bildbreite > 600){
+    document.getElementById("teambild").setAttribute("width", "75%")
+    document.getElementById("teambild2").setAttribute("width", "75%")
+    document.getElementById("teambild3").setAttribute("width", "75%")
+    document.getElementById("teambild4").setAttribute("width", "75%")
+  }
+  else {
+    document.getElementById("teambild").setAttribute("width", "50%")
+    document.getElementById("teambild2").setAttribute("width", "50%")
+    document.getElementById("teambild3").setAttribute("width", "50%")
+    document.getElementById("teambild4").setAttribute("width", "50%")
+  }
 };
 
 /*
@@ -32,30 +55,3 @@ function nav_ine_use() {
     nav.classList.add("inemitdem");
   }
 }
-/*
-Bilder wo uf de Teamsite sind, werded je nach Bildschirmgrössi veränderet
-
-window.onresize = neueBildbreite;
-window.onload = neueBildbreite;
-function neueBildbreite() {
-  Bildbreite = window.innerWidth;
-  if (Bildbreite > 900) {
-    document.getElementById("teambild").setAttribute("width", "300px")
-    document.getElementById("teambild2").setAttribute("width", "300px")
-    document.getElementById("teambild3").setAttribute("width", "300px")
-    document.getElementById("teambild4").setAttribute("width", "300px")
-  }
-  else if (Bildbreite < 900 && Bildbreite > 600){
-    document.getElementById("teambild").setAttribute("width", "75%")
-    document.getElementById("teambild2").setAttribute("width", "75%")
-    document.getElementById("teambild3").setAttribute("width", "75%")
-    document.getElementById("teambild4").setAttribute("width", "75%")
-  }
-  else {
-    document.getElementById("teambild").setAttribute("width", "50%")
-    document.getElementById("teambild2").setAttribute("width", "50%")
-    document.getElementById("teambild3").setAttribute("width", "50%")
-    document.getElementById("teambild4").setAttribute("width", "50%")
-  }
-}
-*/
