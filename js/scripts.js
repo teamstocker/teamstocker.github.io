661fc0c8bd78b506bf64c9423727af486d3e06f7
thebauzz <32292938+thebauzz@users.noreply.github.com>
2018-07-31 06:15:26 +0200

    huge update.. - v180731-06.15

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,47 @@
-
 window.onresize = displayWindowSize;
 window.onload = displayWindowSize;
 function displayWindowSize() {
 
-/* De Wiss schissdräck underem header Bild --> VIlL Z'LANG GHA FÜR DE GAGIFURZ!!!!!!!! */
-  breite = window.innerWidth;
-  var koordinate = "0,50 " + breite + ",50 " + breite + ",0";
-  document.getElementById("zeichnebitte").setAttribute("points", koordinate);
-
 /* Bilder wo uf de Teamsite sind, werded je nach Bildschirmgrössi veränderet */
   Bildbreite = window.innerWidth;
   if (Bildbreite > 900) {
-    document.getElementById("teambild").setAttribute("width", "300px")
-    document.getElementById("teambild2").setAttribute("width", "300px")
-    document.getElementById("teambild3").setAttribute("width", "300px")
-    document.getElementById("teambild4").setAttribute("width", "300px")
+    document.getElementById("teambild").setAttribute("width", "300px");
+    document.getElementById("teambild2").setAttribute("width", "300px");
+    document.getElementById("teambild3").setAttribute("width", "300px");
+    document.getElementById("teambild4").setAttribute("width", "300px");
   }
   else if (Bildbreite < 900 && Bildbreite > 600){
-    document.getElementById("teambild").setAttribute("width", "75%")
-    document.getElementById("teambild2").setAttribute("width", "75%")
-    document.getElementById("teambild3").setAttribute("width", "75%")
-    document.getElementById("teambild4").setAttribute("width", "75%")
+    document.getElementById("teambild").setAttribute("width", "75%");
+    document.getElementById("teambild2").setAttribute("width", "75%");
+    document.getElementById("teambild3").setAttribute("width", "75%");
+    document.getElementById("teambild4").setAttribute("width", "75%");
   }
   else {
-    document.getElementById("teambild").setAttribute("width", "50%")
-    document.getElementById("teambild2").setAttribute("width", "50%")
-    document.getElementById("teambild3").setAttribute("width", "50%")
-    document.getElementById("teambild4").setAttribute("width", "50%")
+    document.getElementById("teambild").setAttribute("width", "50%");
+    document.getElementById("teambild2").setAttribute("width", "50%");
+    document.getElementById("teambild3").setAttribute("width", "50%");
+    document.getElementById("teambild4").setAttribute("width", "50%");
   }
 };
 
-/*
-die erste 3 Ziile mached, dass s'Hamburgermenu zumne X wird.
-De Rest füegt oder entfernt d'Animation zum NAV inetue oder usetue
-*/
+//  breite = window.innerWidth;
+//  var koordinate = "0,50 " + breite + ",50 " + breite + ",0";
+//  document.getElementById("zeichnebitte").setAttribute("points", koordinate);
 
 function nav_ine_use() {
 
   nav = document.getElementById("navfuerjs");
+  container = document.getElementById("emptycontainer");
 
+// entfernt display:none beim emptycontainer
+  container.classList.remove("nope");
+
+// mach das Hamburgermenu zum X und zurück
   document.getElementById("normal1").classList.toggle("gedreht1");
   document.getElementById("normal2").classList.toggle("gedreht2");
   document.getElementById("normal3").classList.toggle("gedreht3");
 
+// entweder einfüge-animation oder entfern-animation der Navigation
   if (nav.classList.contains("inemitdem")) {
     nav.classList.remove("inemitdem");
     nav.classList.add("usemitdem");
@@ -54,4 +53,24 @@ function nav_ine_use() {
   else {
     nav.classList.add("inemitdem");
   }
+}
+
+function byenav() {
+  nav = document.getElementById("navfuerjs");
+  container = document.getElementById("emptycontainer");
+
+// entfernen oder hinzufügen von display:none beim emptycontainer
+  if (container.classList.contains("nope")) {
+    container.classList.remove("nope");
+  }
+  else {
+    container.classList.add("nope");
+  }
+
+// das X zurück zum Hamburgermenu und raus mit der Navigation
+  nav.classList.remove("inemitdem");
+  nav.classList.add("usemitdem");
+  document.getElementById("normal1").classList.toggle("gedreht1");
+  document.getElementById("normal2").classList.toggle("gedreht2");
+  document.getElementById("normal3").classList.toggle("gedreht3");
 }
